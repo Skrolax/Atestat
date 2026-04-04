bbae6cdae6ce98edeb74a246a242154cd9f6ac5f
Karlson <nedelcuradu07@gmail.com>
2026-04-04 14:46:13 +0300

    first commit - test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.socketprogramming.atestat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.socketprogramming.atestat to javafx.fxml;
+    exports com.socketprogramming.atestat;
+}
